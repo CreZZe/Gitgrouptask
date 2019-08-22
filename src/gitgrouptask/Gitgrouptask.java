6e47ef4fb2c4be77c5adbcdfac5576f643e4a33e
Mikael Fredriksson <224Mikael@gmail.com>
2019-08-22 13:32:54 +0200

    Konflikt plis

--- src/gitgrouptask/Gitgrouptask.java
+++ src/gitgrouptask/Gitgrouptask.java
@@ -5,7 +5,8 @@ public class Gitgrouptask {
 
     public static void main(String[] args) {
 
-
+        System.out.println("MIKAEL");
+        
         PalindromeChecker checker = new PalindromeChecker();
         System.out.println(checker.checker("aNna"));
     }
