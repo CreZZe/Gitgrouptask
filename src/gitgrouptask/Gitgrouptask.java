c0c94908cb6a0c36941dee4f3e92d706f2aa6aee
David Sodergren <david.sodergren94@gmail.com>
2019-08-22 13:31:24 +0200

    risc conflict

--- src/gitgrouptask/Gitgrouptask.java
+++ src/gitgrouptask/Gitgrouptask.java
@@ -5,8 +5,7 @@ public class Gitgrouptask {
 
     public static void main(String[] args) {
 
-        System.out.println("hej");
-        
+
         PalindromeChecker checker = new PalindromeChecker();
         System.out.println(checker.checker("aNna"));
     }
